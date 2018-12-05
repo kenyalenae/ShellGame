bdaffe3137f85bed17996e413d45a317822c7813
ag5300cm <bhillenbrand101@gmail.com>
2018-12-05 16:19:33 -0600

    Update Item.cs

--- CoreLogic/CoreLogic/Item.cs
+++ CoreLogic/CoreLogic/Item.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// used for round logic, will assist checking if you already clicked on a box on the table 
 namespace CoreLogic
 {
     public class Item
