5d12f188e12168f12c3ae0f72834a2fd3b582b9d
ag5300cm <bhillenbrand101@gmail.com>
2018-12-05 21:40:07 -0600

    Update HighScore.cs

--- ShellGame2/Assets/Scripts/HighScore.cs
+++ ShellGame2/Assets/Scripts/HighScore.cs
@@ -10,19 +10,20 @@ public static class HighScore
         get { return highScore; }
         set
         {
-            if (value > highScore)
+            if (value > highScore)  // if value is greater than current high score change it
             {
                 highScore = value;
             }
         }
     }
 
-    public static void LoadHighScore()
+    public static void LoadHighScore()  // When you start up the game, will load the highest score ever got 
     {
+        //                                         \/ if game never yet be played, load this default 
         highScore = PlayerPrefs.GetInt("HighScore", 0);
     }
 
-    public static void SaveHighScore()
+    public static void SaveHighScore()  // Will save high score when game shut down 
     {
         PlayerPrefs.SetInt("HighScore", highScore);
     }
