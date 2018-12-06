f19f9c03ba7989d48b1fb206d6bc6439ae0bd30a
ag5300cm <bhillenbrand101@gmail.com>
2018-12-05 19:42:59 -0600

    Update ItemInput.cs

--- ShellGame2/Assets/Scripts/ItemInput.cs
+++ ShellGame2/Assets/Scripts/ItemInput.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 
 public class ItemInput : MonoBehaviour, IInputClickHandler
 {
+    // \/ tells unity to take info into ram for faster gaming
     [SerializeField]
     private int itemId;
 
+    //            \/ used when you tap an item (box/shell) in the game, such as when you click the trigger 
     public void OnInputClicked(InputClickedEventData eventData)
     {
+        // method in GameController that does if user clicked 
         GameController.Instance.CheckForItem(itemId);
     }
 
